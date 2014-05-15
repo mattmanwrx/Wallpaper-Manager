9d7b47b296ea54c32db3d6f98f0f1bf2be79864d
mattmanwrx <mbloomfield87@gmail.com>
2014-05-15 12:37:21 +0100

    BOW2: WIP for V2 - still WIP
    Signed-off-by: mattmanwrx <mbloomfield87@gmail.com>

---
+++ src/com/jb/core/OnFragmentClickListener.java
@@ -0,0 +1,9 @@
+package com.jb.core;
+
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+
+public interface OnFragmentClickListener extends OnNavigationListener {
+	public void onCategorySelected (NodeCategory node);
+
+	public void onWallpaperSelected (NodeWallpaper node);
+}
--- src/com/wallpaper/core/OnFragmentClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.wallpaper.core;
-
-import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
-
-public interface OnFragmentClickListener extends OnNavigationListener {
-	public void onCategorySelected (NodeCategory node);
-
-	public void onWallpaperSelected (NodeWallpaper node);
-}
